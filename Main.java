28694450e97876ecad5a3ed7ff696e315b95adef
Guilherme Avelino <gavelino@gmail.com>
2023-01-10 09:00:36 -0300

    implement issue #1

--- Main.java
+++ Main.java
@@ -3,8 +3,12 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
+		int option = -1;
+		while (option != 4){
+			option = mainMenu.getSelection();
+		}
+		System.out.println(option + "foi selecionada");
 		System.out.println("Fim");
 	}
 
